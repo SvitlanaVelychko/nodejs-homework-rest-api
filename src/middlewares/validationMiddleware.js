103617f4c3caee913da6c64603c74e6aa4b93e95
SvitlanaVelychko <svel2608@gmail.com>
2022-10-29 03:39:20 +0300

    Created rest-api by express

---
+++ src/middlewares/validationMiddleware.js
@@ -0,0 +1,46 @@
+const Joi = require('joi')
+
+module.exports = {
+    addContactValidation: (req, res, next) => {
+        const schema = Joi.object({
+            name: Joi.string()
+                .min(3)
+                .max(30)
+                .required(),
+            email: Joi.string()
+                .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
+                .required(),
+            phone: Joi.string()
+                .min(5)
+                .max(15)
+                .required(),
+        })
+
+        const validationlResult = schema.validate(req.body)
+        if (validationlResult.error) {
+            return res.status(400).json({status:validationlResult.error.details})
+        }
+        next()
+    },
+    updateContactValidation: (req, res, next) => {
+        const schema = Joi.object({
+            name: Joi.string()
+                .min(3)
+                .max(30)
+                .optional(),
+            email: Joi.string()
+                .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
+                .optional(),
+            phone: Joi.string()
+                .min(5)
+                .max(15)
+                .optional(),
+        }).min(1)
+
+        const validationlResult = schema.validate(req.body)
+        if (validationlResult.error) {
+            return res.status(400).json({status:validationlResult.error.details})
+        }
+        next()
+    },
+}
